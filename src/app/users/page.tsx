7c0dbb31200f0a79df7fbefa9da22981019c3eee
LeandroOliveiraDev <lleandro.silva@outlook.com>
2024-12-31 17:45:56 -0300

    table user

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,12 +2,33 @@
 
 import { api } from "@/lib/api";
 import { useEffect, useState } from "react";
-
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 interface User {
     id: number
     name: string
     email: string
+    phoneNumber: string
+    address: string
 }
 
 export default function UsersPage() {
@@ -20,12 +41,64 @@ export default function UsersPage() {
     }, [])
 
     return (
-        <div>
-            {
-                users.map(user => (
-                    <p key={user.id}>{user.name}</p>
-                ))
-            }
-        </div>
+        <>
+            <Dialog>
+                <div className="p-8">
+                    <h1 className="mb-4 text-2xl">Users</h1>
+                    <div className="flex flex-col">
+                        <DialogTrigger asChild>
+                            <Button size="sm" className="flex-1 self-end">NEW</Button>
+                        </DialogTrigger>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>email</TableHead>
+                                    <TableHead>name</TableHead>
+                                    <TableHead>phoneNumber</TableHead>
+                                    <TableHead>address</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {users.map((user, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell className="font-medium">{user.email}</TableCell>
+                                        <TableCell className="font-medium">{user.name}</TableCell>
+                                        <TableCell className="font-medium">{user.phoneNumber}</TableCell>
+                                        <TableCell className="font-medium">{user.address}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
+
+
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-right">
+                                Name
+                            </Label>
+                            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                                Username
+                            </Label>
+                            <Input id="username" value="@peduarte" className="col-span-3" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
     )
 }
